5b8957ac52027d7e9858b7ee8d29ce3798bcd5f7
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-04-17 22:24:54 +0300

    add based page

---
+++ cypress/page-objects/page/BasePage.js
@@ -0,0 +1,8 @@
+export default class BasePage {
+    constructor() {
+        this.baseUrl = 'https://www.saucedemo.com/';
+    }
+    open(url) {
+        cy.visit(`${this.baseUrl}${url}`);
+    }
+}
