ba2acd8b8ec9cbfb22412080095fc2490e0ce401
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-04-17 22:23:34 +0300

    added new data

---
+++ cypress/page-objects/page/ItemPage.js
@@ -0,0 +1,31 @@
+import LoginPage from "./LoginPage";
+import InventoryPage from "./InventoryPage";
+import BasePage from "./BasePage";
+
+import userData from '../../fixtures/userData.json'
+
+class ItemPage extends BasePage {
+
+   get itemImg(){
+       return cy.get('[data-test="item-sauce-labs-onesie-img"]')
+   }
+   get itemDiscribe(){
+    return cy.get('[data-test="inventory-item-desc"]')
+   }
+   get itemName(){
+    return cy.get('[data-test="inventory-item-name"]')
+    
+   }
+   get itemPrice(){
+    return cy.get('[data-test="inventory-item-price"]')
+     
+   }
+
+   open(){
+    super.open('')
+    LoginPage.login(userData.userNames.correctUser, userData.passwords.correctPassword)
+}
+
+}
+
+export default new ItemPage();
